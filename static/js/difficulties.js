4dacde73ae76da41ea89e1c84ec570168e210b79
kate <kate@evercharge.net>
2015-06-24 16:36:32 -0700

    working on tabbing btwn ev owner and bldg management with bs builtins

--- static/js/difficulties.js
+++ static/js/difficulties.js
@@ -3,3 +3,9 @@ $('h2.difficulties').hover(function() {
 	var item_id = $(this).attr('id');
 	$('div#'+ item_id).toggle();
 })
+
+
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
