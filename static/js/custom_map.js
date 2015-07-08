9ef689c6668c4e8f173e2982ef67ba273bb1cbf6
kate <kate@evercharge.net>
2015-07-07 18:16:42 -0700

    updated google map to include sf location and added infoboxes - fixed power management content

--- static/js/custom_map.js
+++ static/js/custom_map.js
@@ -1,5 +1,12 @@
 var map;
 var emeryville = new google.maps.LatLng(37.825187, -122.289265);
+var bolt = new google.maps.LatLng(37.800081, -122.397838);
+var bay = new google.maps.LatLng(37.813098, -122.359018)
+
+var emeryString = '<h3>3425 Ettie Street</h3>'+
+                  'Emeryville, CA 94608'
+var boltString = '<h3>PIER 9</h3>'+
+      'San Francisco, CA 94111'
 
 var MY_MAPTYPE_ID = 'custom_style';
 
@@ -30,7 +37,7 @@ function initialize() {
 
   var mapOptions = {
     zoom: 13,
-    center: emeryville,
+    center: bay,
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
     },
@@ -50,10 +57,31 @@ function initialize() {
     name: 'Evercharge Custom Style'
   };
 
-  var marker = new google.maps.Marker({
+  var emeryMarker = new google.maps.Marker({
       position: emeryville,
       map: map,
-      title: 'Hello World!'});
+      title: 'Emeryville Office'});
+  
+  var emeryWindow = new google.maps.InfoWindow({
+      content: emeryString
+  });
+
+    var boltMarker = new google.maps.Marker({
+      position: bolt,
+      map: map,
+      title: 'SF Office'});
+
+  var boltWindow = new google.maps.InfoWindow({
+      content: boltString
+  });
+
+  google.maps.event.addListener(emeryMarker, 'click', function() {
+    emeryWindow.open(map,emeryMarker);
+  });
+
+  google.maps.event.addListener(boltMarker, 'click', function() {
+    boltWindow.open(map,boltMarker);
+  });
 
   var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
 
