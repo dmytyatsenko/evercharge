ee06673a5ad84000e274dfdc045d47404eca337a
kate <kate@evercharge.net>
2015-07-08 17:20:13 -0700

    updated navbar js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,16 +1,15 @@
 $(window).scroll(
-    {
-        previousTop: 0
-    }, 
-    function () {
-    var currentTop = $(window).scrollTop();
-    if (currentTop < this.previousTop) {
-        $(".navbar-fixed-top").fadeIn();
+	{
+		previousTop: 0
+	}, 
+	function () {
+	var currentTop = $(window).scrollTop();
+	if (currentTop > this.previousTop) {
+		$(".navbar-fixed-top").fadeOut();
 
-    } else {
-        $(".navbar-fixed-top").fadeOut();
+	} else {
+		$(".navbar-fixed-top").fadeIn();
 
-
-    }
-    this.previousTop = currentTop;
+	}
+	this.previousTop = currentTop;
 });
