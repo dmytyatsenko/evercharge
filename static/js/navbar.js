fdd324dc522d0ca53d38dcb54188cbf2187a5900
kate <kate@evercharge.net>
2015-07-10 12:22:04 -0700

    fixed jank color on navbar focus, disable javascript that faded navbar on mobile

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,15 +1,31 @@
-$(window).scroll(
-	{
-		previousTop: 0
-	}, 
-	function () {
-	var currentTop = $(window).scrollTop();
-	if (currentTop > this.previousTop) {
-		$(".navbar-fixed-top").fadeOut();
 
-	} else {
-		$(".navbar-fixed-top").fadeIn();
+var fadeNavbar = function() {
 
-	}
-	this.previousTop = currentTop;
-});
+	$(window).scroll(
+		{
+			previousTop: 0
+		}, 
+		function () {
+		var currentTop = $(window).scrollTop();
+		if (currentTop > this.previousTop) {
+			$(".navbar-fixed-top").fadeOut();
+
+		} else {
+			$(".navbar-fixed-top").fadeIn();
+
+		}
+		this.previousTop = currentTop;
+	});
+
+}
+
+
+//Checking for mobile to disable navbar fade
+
+
+if($(window).width() < 480) { 
+    // do any 480 width stuff here, or simply do nothing
+    var mobile = true;
+} else {
+	fadeNavbar();
+}
