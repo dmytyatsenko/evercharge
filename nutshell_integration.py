578426772a23b0393e7e1569e5a4f7ba6c035552
dayee <dayee@evercharge.net>
2016-01-26 16:53:58 -0800

    FIX: commented out code for removed customField in nutshell

--- nutshell_integration.py
+++ nutshell_integration.py
@@ -35,7 +35,8 @@ def add_new_contact(name, email, phone, address1,
 
 def add_new_lead(contact_id, source, note=None, buildingSize=None):
 	new_lead = n.newLead(contacts=[{'id': contact_id}], sources=[{'id': source}],
-	note=note, customFields={'Approximate Bldg Size': buildingSize})
+	note=note)
+	# customFields={'Approximate Bldg Size': buildingSize}
 
 	return new_lead
 
