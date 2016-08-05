15db15e9c0ce772bc3128cc6ad9381856bd46fea
Nikolay Golub <nikolay.v.golub@gmail.com>
2016-08-05 15:43:42 +0300

    WEB-80 : Remove unused javascript files

--- static/js/transition.js
+++ static/js/transition.js
@@ -21,7 +21,7 @@
       MozTransition    : 'transitionend',
       OTransition      : 'oTransitionEnd otransitionend',
       transition       : 'transitionend'
-    }
+    };
 
     for (var name in transEndEventNames) {
       if (el.style[name] !== undefined) {
@@ -34,18 +34,18 @@
 
   // http://blog.alexmaccaw.com/css-transitions
   $.fn.emulateTransitionEnd = function (duration) {
-    var called = false
-    var $el = this
+    var called = false;
+    var $el = this;
     $(this).one('bsTransitionEnd', function () { called = true })
     var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
-    setTimeout(callback, duration)
+    setTimeout(callback, duration);
     return this
-  }
+  };
 
   $(function () {
     $.support.transition = transitionEnd()
 
-    if (!$.support.transition) return
+    if (!$.support.transition) return;
 
     $.event.special.bsTransitionEnd = {
       bindType: $.support.transition.end,
