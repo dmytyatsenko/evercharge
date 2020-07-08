f56514c8c80a7d32061c0cb8f5829a2e83f67e4b
Kristin Germeroth <kristinG@evercharge.net>
2020-07-08 11:05:41 -0700

    qr code scanner functional

--- static/js/qr_code_reader.js
+++ static/js/qr_code_reader.js
@@ -1,4 +1,4 @@
-const qrcode = window.qrcode;
+var qrcode = window.qrcode;
 
 const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
@@ -10,51 +10,49 @@ const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
 
-qrcode.callback = (res) => {
-    if (res) {
-        outputData.innerText = res;
-        scanning = false;
+qrcode.callback = res => {
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
 
-        video.srcObject.getTracks().forEach(track => {
-            track.stop();
-        });
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
 
-        qrResult.hidden = false;
-        btnScanQR.hidden = false;
-        canvasElement.hidden = true;
-    }
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+  }
+};
+
+btnScanQR.onclick = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
 };
 
 function tick() {
-    canvasElement.height = video.videoHeight;
-    canvasElement.width = video.videoWidth;
-    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
 
-    scanning && requestAnimationFrame(tick);
+  scanning && requestAnimationFrame(tick);
 }
 
 function scan() {
-    try {
-        console.log('trying to decode qr code');
-        qrcode.decode();
-    } catch (e) {
-        setTimeout(scan, 300);
-    }
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
 }
-
-btnScanQR.onclick = () =>
-    console.log('hello!');
-    navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: "environment" } })
-    .then(function(stream) {
-        scanning = true;
-        qrResult.hidden = true;
-        btnScanQR.hidden = true;
-        canvasElement.hidden = false;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        video.srcObject = stream;
-        video.play();
-        tick();
-        scan();
-    });
-
